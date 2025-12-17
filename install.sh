8fcd1944bba4b3a261bdf2c30c6f077fa6aee02a
Shane Lupton <shanelupton@hurricaneprint.com>
2025-12-17 17:19:15 -0600

    revised install script and data locations

--- install.sh
+++ install.sh
@@ -1,40 +1,28 @@
 #! /bin/sh
+SERVICEDIRECTORY="./service"
 
 #stop and remove service if previous version exists
-if test -f "/etc/init.d/filamentsyncimprovement"; then
-    /etc/init.d/filamentsyncimprovement disable
-    /etc/init.d/filamentsyncimprovement stop
-    rm /etc/init.d/filamentsyncimprovement
-fi
-
 if test -f "/etc/init.d/filamentsync"; then
+    echo "Removing previous version"
     /etc/init.d/filamentsync disable
     /etc/init.d/filamentsync stop
     rm /etc/init.d/filamentsync
 fi
 
 #create data dir
-mkdir -p data
+mkdir -p /usr/share/Filament-Sync
 
 #install and enable startup service
-if test -f "/mnt/UDISK/root/printer_data/config/Filament-Sync-Service/service/sync.sh"; then
-    SERVICEDIRECTORY="/mnt/UDISK/root/printer_data/config/Filament-Sync-Service/service"
-    cp ${SERVICEDIRECTORY}/filamentsyncimprovement /etc/init.d/
-    chmod +x ${SERVICEDIRECTORY}/sync.sh
-    chmod +x /etc/init.d/filamentsyncimprovement
-    /etc/init.d/filamentsyncimprovement enable
-    /etc/init.d/filamentsyncimprovement start
+if test -f "/opt/lib/sftp-server"; then
+    echo "SFTP already installed"
 else
-    SERVICEDIRECTORY="/mnt/UDISK/printer_data/config/Filament-Sync-Service/service"
-    cp ${SERVICEDIRECTORY}/filamentsync /etc/init.d/
-    chmod +x ${SERVICEDIRECTORY}/sync.sh
-    chmod +x /etc/init.d/filamentsync
-    /etc/init.d/filamentsync enable
-    /etc/init.d/filamentsync start
     echo "Installing SFTP"
     opkg install ${SERVICEDIRECTORY}/openssh-sftp-server_10.0_p1-1_armv7-3.2.ipk
 fi
 
-echo "Ready to sync"
-echo "Make sure the script is set in your slicers post-processing options"
-echo "or in terminal run 'node main.js' in the filament-sync directory"
+cp ${SERVICEDIRECTORY}/filamentsync /etc/init.d/
+chmod +x ${SERVICEDIRECTORY}/sync.sh
+chmod +x /etc/init.d/filamentsync
+/etc/init.d/filamentsync enable
+/etc/init.d/filamentsync start
+echo "Service is" `/etc/init.d/filamentsync status`
--- service/sync.sh
+++ service/sync.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 CREALITYDIRECTORY="/mnt/UDISK/creality/userdata/box"
-
-if test -f "/mnt/UDISK/root/printer_data/config/Filament-Sync-Service/service/sync.sh"; then
-    SYNCDIRECTORY="/mnt/UDISK/root/printer_data/config/Filament-Sync-Service/data"
-else
-    SYNCDIRECTORY="/mnt/UDISK/printer_data/config/Filament-Sync-Service/data"
-fi
+SYNCDIRECTORY="/usr/share/Filament-Sync"
 
 while :
 do
