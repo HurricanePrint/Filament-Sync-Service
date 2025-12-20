cf5417c0d622f0ece7c7818259d6f147cafb498c
Shane Lupton <shanelupton@hurricaneprint.com>
2025-12-20 13:57:59 -0600

    fixed issue adding to service file

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ echo "Service is" `/etc/init.d/filamentsync status`
 SERVICEFILE="/mnt/UDISK/printer_data/moonraker.asvc"
 SERVICELINE="filamentsync"
 
-grep -qFx "$SERVICELINE" "$SERVICEFILE" || echo -e "$SERVICELINE" >> "$SERVICEFILE"
+grep -qxF 'filamentsync' ~/printer_data/moonraker.asvc || { sed -i '$a\' ~/printer_data/moonraker.asvc; echo "filamentsync" >> ~/printer_data/moonraker.asvc; }
 
 CONFFILE="/mnt/UDISK/printer_data/config/moonraker.conf"
 CONFBLOCK="[update_manager filamentsync]"
